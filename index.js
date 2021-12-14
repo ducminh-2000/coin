eb0d3759f2a7eaf39318b8535aa43e09a8763645
duc08 <duc08@DESKTOP-NVG2TNG>
2021-12-14 15:32:17 +0700

    duc

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get(['/'],(req,res) => {
     res.sendFile(path.join(__dirname,'public/trending.html'));
 })
+app.get(['/landing'],(req,res) => {
+    res.sendFile(path.join(__dirname,'public/landingpage.html'));
+})
 
 const port = process.env.PORT || 3000; 
 
